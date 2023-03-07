e06c7738d62918b4316635175bda70f8a0a146fe
Gazella <edison19951219@gmail.com>
2023-03-07 09:14:53 +0800

    Navbar section

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from '@/styles/Navbar.module.css'
+import { BsFacebook, BsInstagram, BsGithub} from 'react-icons/bs';
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar_main}> 
+        <div className={styles.navbar_logo}>
+            <img src='./logo.png'/>
+        </div>
+        <ul className={styles.navbar_list}>
+            <li>
+                Blog
+            </li>
+            <li>
+                About
+            </li>
+            <li>
+                Contact
+            </li>
+        </ul>
+        <div className={styles.navbar_icons}>
+            <BsFacebook className={styles.navbar_icon}/>
+            <BsInstagram className={styles.navbar_icon}/>
+            <BsGithub className={styles.navbar_icon}/>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
