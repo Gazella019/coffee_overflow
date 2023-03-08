57fe92411a330a4bf213c52ee4594f0792d619a2
Gazella <edison19951219@gmail.com>
2023-03-08 09:34:18 +0800

    Hero section

---
+++ components/Hero/Hero.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Splide, SplideTrack, SplideSlide } from '@splidejs/react-splide';
+import styles from  '@/styles/Hero.module.css';
+import '@splidejs/react-splide/css';
+
+
+{/* <img src="./images/hero2.jpg" alt="Image 2"/>
+<img src="./images/hero2.jpg" alt="Image 2"/> */}
+
+const Hero = () => {
+  return (
+    <div>
+        <Splide hasTrack={ false }
+        options={ {
+            rewind: true,
+        } }
+        className={styles.splide}
+        >
+            <SplideTrack>
+                <SplideSlide className={styles.splide_slide}>
+                    <img src="./images/hero2.jpg" alt="Image 2"/>
+                    <p></p>
+                </SplideSlide>
+                <SplideSlide className={styles.splide_slide}>
+                    <img src="./images/hero1.jpg" alt="Image 2"/>
+                </SplideSlide>
+            </SplideTrack>
+        </Splide>
+    </div>
+  )
+}
+
+export default Hero
