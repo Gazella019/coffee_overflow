224897df50e9cf6d1d8f052b236262c3dc895e3f
Gazella <edison19951219@gmail.com>
2023-03-25 10:58:22 +0800

    Mdx bundler system

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -2,17 +2,20 @@ import React from 'react'
 import { getMDXComponent } from 'mdx-bundler/client'
 import { getAllPostIds, getPostMdxData } from '../../lib/posts'
 import Layout from '@/components/Layout/Layout'
+import CategoryCardOne from '@/components/CategoryCard/CategoryCardOne'
 
-const Post = ( {mdxData} ) => {
-    const Component = React.useMemo(() => getMDXComponent(mdxData.code), [mdxData.code])
+const Post = ( {mdxData: {slug, code, frontmatter}} ) => {
+    const Component = React.useMemo(() => getMDXComponent(code), [code])
     return (
         <Layout>
             <div>
-                <h1>{mdxData.frontmatter.title}</h1>
-                <p>{mdxData.frontmatter.description}</p>
-                <p>{mdxData.slug}</p>
+                <h1>{frontmatter.title}</h1>
+                <p>{frontmatter.description}</p>
+                <p>{slug}</p>
                 <div>
-                    <Component />
+                    <Component 
+                        components={{CategoryCardOne}}
+                    />
                 </div>
             </div>
         </Layout>
