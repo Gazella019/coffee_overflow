16a12561666d51ac53a14017f210d92cfe72ccdd
Gazella <edison19951219@gmail.com>
2023-09-21 15:33:59 +0800

    Add typography js setting

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import '@/styles/globals.css'
 import '@/styles/prism.css'
+import { ThemeUIProvider } from 'theme-ui';
+import { toTheme } from '@theme-ui/typography'
 import Layout from '@/components/Layout/Layout'
-// import { ThemeUIProvider } from 'theme-ui'
+import githubTheme from 'typography-theme-github'
+import noriegaTheme from 'typography-theme-noriega'
 import { motion, AnimatePresence } from 'framer-motion'
 
 
 export default function App({ Component, pageProps, router }) {
+  // const theme = toTheme(noriegaTheme)
+  const theme = toTheme(githubTheme)
   return (
+    <ThemeUIProvider theme={theme}>
       <Layout>
         <AnimatePresence initial={false}
           mode="wait"
@@ -21,5 +27,6 @@ export default function App({ Component, pageProps, router }) {
           {/* </motion.div> */}
         </AnimatePresence>
       </Layout>
+    </ThemeUIProvider>
   )
 }
